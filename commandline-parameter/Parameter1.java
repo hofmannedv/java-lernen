0774c9943a329c0c3f0b649342e14ab571cec95d
Frank Hofmann <frank.hofmann@efho.de>
2025-12-30 00:54:24 +0100

    handle commandline parameters: output first parameter, only

---
+++ commandline-parameter/Parameter1.java
@@ -0,0 +1,11 @@
+// (C) 2025 Frank Hofmann, email info@efho.de
+// Licensed under General Public License (GPL)
+
+// demonstrates how to evaluate, and output the first parameter
+// usage: java Parameter1 value
+
+public class Parameter1 {
+  public static void main (String[] args) {
+    System.out.println("Hello, the given parameter is: '" + args[0] + "'");
+  }
+}
