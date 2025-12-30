092fc1917c56c700928bfe16198b909e07e156b3
Frank Hofmann <frank.hofmann@efho.de>
2025-12-30 17:52:39 +0100

    add examples with comments

---
+++ comments/Comments.java
@@ -0,0 +1,20 @@
+// (C) 2025 Frank Hofmann, email info@efho.de
+// Licensed under General Public License (GPL)
+
+// demonstrates how to comment your code
+// usage: java Comments
+
+public class Comments {
+  public static void main (String[] args) {
+    
+    // comment on a single line
+    int postcode;            // comment next to code
+
+    // multi-line comments
+    /* this is a comment
+       that spans over
+       three lines */
+    boolean atHome = true;   // boolean variable pre-set with True
+    boolean atWork = false;  // boolean variable pre-set with False
+  }
+}
