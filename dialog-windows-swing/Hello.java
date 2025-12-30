f5ff51de61dcb9e039cb82bd6c326334788ada4d
Frank Hofmann <frank.hofmann@efho.de>
2025-12-30 19:02:13 +0100

    Add SPDX identifier

--- dialog-windows-swing/Hello.java
+++ dialog-windows-swing/Hello.java
@@ -1,8 +1,15 @@
-// (C) 2025 Frank Hofmann, email info@efho.de
-// Licensed under General Public License (GPL)
-
+// ------------------------------------------------------------------
 // demonstrates how to display an output window using SWING
+//
+// (C) 2025 Frank Hofmann
+// Released under GNU Public License (GPL)
+// email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
 // usage: java Hello
+// ------------------------------------------------------------------
 
 // import required Java modules
 import java.awt.*;
