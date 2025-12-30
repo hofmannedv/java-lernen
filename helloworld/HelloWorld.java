d90a32c4cb2bcb2cab447feba1caf6e615260062
Frank Hofmann <frank.hofmann@efho.de>
2025-12-30 01:01:28 +0100

    helloworld: added copyright, and license information

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -1,5 +1,11 @@
+// (C) 2025 Frank Hofmann, email info@efho.de
+// Licensed under General Public License (GPL)
+
+// demonstrates how to output the string "Hello, world!"
+// usage: java HelloWorld
+
 public class HelloWorld {
-  public static void main (String args[]) {
+  public static void main (String[] args) {
     System.out.println("Hello, world!");
   }
 }
