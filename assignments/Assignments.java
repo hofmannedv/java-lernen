8ff3c762e87febc1556a0c86339a398268a32f47
Frank Hofmann <frank.hofmann@efho.de>
2025-12-30 20:46:15 +0100

    assignments, structure

---
+++ assignments/Assignments.java
@@ -0,0 +1,31 @@
+// ------------------------------------------------------------------
+// demonstrates how to assign values to a variable
+//
+// (C) 2025 Frank Hofmann
+// Released under GNU Public License (GPL)
+// email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// usage: java Assignments
+// ------------------------------------------------------------------
+
+public class Assignments {
+  public static void main (String[] args) {
+
+    // single assignments
+
+    // multiple assignments
+
+    // assignments with calculations
+
+    // print a partial string from index position 2 up to, but not including 4
+
+    // print a partial string, the last four letters
+
+    // print a partial string from index position 2 up to the end
+
+    // System.out.println("Hello, world!");
+  }
+}
