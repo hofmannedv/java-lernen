c2d9ce0c51656db955912987b9afc27805b01993
Frank Hofmann <frank.hofmann@efho.de>
2025-12-31 16:56:51 +0100

    assignments, examples for assignments to print substrings, refinement

--- assignments/Assignments.java
+++ assignments/Assignments.java
@@ -53,13 +53,13 @@ public class Assignments {
     System.out.printf("output4: " + output4 + "\n");
     System.out.printf("output5: " + output5 + "\n");
 
-    // print a partial string from the beginning up to index position 2
-    System.out.println(colour.substring(0, 2));   // printr 'bl'
+    // print a partial string from the beginning up to index position 2 ('bl')
+    System.out.println(colour.substring(0, 2));
 
-    // print a partial string, the last three letters
+    // print a partial string, the last three letters ('lue')
     System.out.println(colour.substring(colour.length() - 3, colour.length()));
 
-    // print a partial string from index position 2 up to the end
+    // print a partial string from index position 2 up to the end ('ue')
     System.out.println(colour.substring(2, colour.length()));
   }
 }
