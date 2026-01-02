3063e0b1f2ed810f1183942b851f13246a765ab4
Frank Hofmann <frank.hofmann@efho.de>
2026-01-02 20:49:42 +0100

    arithmetic operators, tidying up

--- operators/ArithmeticOperators.java
+++ operators/ArithmeticOperators.java
@@ -132,17 +132,6 @@ public class ArithmeticOperators {
 
   public static void main (String[] args) {
 
-    // variable assignments
-    boolean isAvailable = true;        // boolean assignment with value 'true'
-    byte amount = 5;                   // byte assignment with value '5' 
-    short width = -45;                 // short assignment with value '-45' 
-    char letter = '%';                 // character assignment with value '%'
-    int postcode = 14467;              // integer assignment with value '14467' 
-    long height = 135792468;           // long assignment with value '135792468' 
-    float weight = 1.25f;              // float assignment with value 1.25
-    double size = 2.25;                // double assignment with value 2.25
-    String colour = "blue";            // string assignment with value 'blue'
-
     // arithmetic operators
     // * negation
     System.out.println("= Negation =");
