cdff27e53520daef428fd07c2a27b85f079e8ee1
Frank Hofmann <frank.hofmann@efho.de>
2026-01-02 22:21:34 +0100

    string operators, check for substring in text

--- operators/StringOperators.java
+++ operators/StringOperators.java
@@ -15,18 +15,37 @@
 public class StringOperators {
   public static void glueStrings(String operand1, String operand2) {
     
-    // glue the operands together using + operator
+    // glue the two operands together using + operator
     String result = operand1 + operand2;
     
     // output result
     System.out.printf("the result of '%s' and '%s' is '%s'. \n", operand1, operand2, result);
   }
 
+  public static void containsString(String text, String searchString) {
+  
+    // does text contain searchString?
+    boolean result = text.contains(searchString);
+    if (result == false) {
+       // searchString is not in text
+       System.out.printf("'%s' cannot be found in '%s' \n", searchString, text);
+    } else {
+       // searchString is in text
+       System.out.printf("'%s' is found in '%s' \n", searchString, text);
+    }
+  }
+
   public static void main (String[] args) {
 
     // glue two strings together using + operator
     System.out.println("= Glue Two Strings Together =");
-    glueStrings("Cape ", "Town");      // use +
+    glueStrings("Cape ", "Town");           // use +
+    System.out.println(" ");
+
+    // check for substring in text
+    System.out.println("= Check For Substring =");
+    containsString("Cape Town", "Cape");    // will return true
+    containsString("Cape Town", "Canal");   // will return false
     System.out.println(" ");
 
   }
