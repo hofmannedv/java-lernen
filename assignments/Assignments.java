6aa1d60b11786e1556b9e66d83eb87ef59fa9bca
Frank Hofmann <frank.hofmann@efho.de>
2026-01-05 12:31:32 +0100

    assignments: adjusted documentation

--- assignments/Assignments.java
+++ assignments/Assignments.java
@@ -1,14 +1,14 @@
 // ------------------------------------------------------------------
-// demonstrates how to assign values to a variable
+// Demonstrates how to assign values to a variable
 //
 // (C) 2025-2026 Frank Hofmann
 // Released under GNU Public License (GPL)
-// email frank.hofmann@efho.de
+// Email frank.hofmann@efho.de
 // 
 // License: GNU Public License (GPL) v.3.0 or later
 // SPDX-License-Identifier: GNU General Public License v3.0 or later
 // 
-// usage: java Assignments
+// Usage: java Assignments
 // ------------------------------------------------------------------
 
 public class Assignments {
