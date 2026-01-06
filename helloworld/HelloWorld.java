5d6a7d1c352100225fdbb25f2f66a8c67711696b
Frank Hofmann <frank.hofmann@efho.de>
2026-01-06 02:18:10 +0100

    helloworld: update documentation

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -1,14 +1,14 @@
 // ------------------------------------------------------------------
-// demonstrates how to output the string "Hello, world!"
+// Demonstrates how to output the string "Hello, world!"
 //
 // (C) 2025 Frank Hofmann
 // Released under GNU Public License (GPL)
-// email frank.hofmann@efho.de
+// Email frank.hofmann@efho.de
 // 
 // License: GNU Public License (GPL) v.3.0 or later
 // SPDX-License-Identifier: GNU General Public License v3.0 or later
 // 
-// usage: java HelloWorld
+// Usage: java HelloWorld
 // ------------------------------------------------------------------
 
 public class HelloWorld {
