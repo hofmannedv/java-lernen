674476627b92d21fa9e64fa8103f77fc78728556
Frank Hofmann <frank.hofmann@efho.de>
2026-01-09 15:39:57 +0100

    datasets: added basic class for distribution element

---
+++ datasets/DistributionElement.java
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------------
+// Defines a class containing two elements for value, and counter
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: java DistributionElement
+//
+// ------------------------------------------------------------------
+
+public class DistributionElement {
+  int value;                           // value to be counted
+  int counter;                         // number of items
+}
