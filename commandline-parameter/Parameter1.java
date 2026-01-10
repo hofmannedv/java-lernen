3b0b8086b421395424c79e29a770d6e3bd47cb7c
Frank Hofmann <frank.hofmann@efho.de>
2026-01-10 14:21:09 +0100

    commandline parameters: refined call, and documentation

--- commandline-parameter/Parameter1.java
+++ commandline-parameter/Parameter1.java
@@ -1,18 +1,19 @@
 // ------------------------------------------------------------------
-// demonstrates how to evaluate, and output the first parameter
+// Demonstrates how to evaluate, and output the first parameter
 //
 // (C) 2025 Frank Hofmann
 // Released under GNU Public License (GPL)
-// email frank.hofmann@efho.de
+// Email frank.hofmann@efho.de
 // 
 // License: GNU Public License (GPL) v.3.0 or later
 // SPDX-License-Identifier: GNU General Public License v3.0 or later
 // 
-// usage: java Parameter1 value
+// Usage: java Parameter1 value
+// Subsitute 'value' by the actual value of the call parameter
 // ------------------------------------------------------------------
 
 public class Parameter1 {
   public static void main (String[] args) {
-    System.out.println("Hello, the given parameter is: '" + args[0] + "'");
+    System.out.println("this program has been called with the following parameter: '" + args[0] + "'");
   }
 }
