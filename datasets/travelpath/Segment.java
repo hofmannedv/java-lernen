2e95b1853114137bcf0a6ce8457c64eefaaec544
Frank Hofmann <frank.hofmann@efho.de>
2026-01-13 22:53:45 +0100

    datasets: add Segments class for travelpath

---
+++ datasets/travelpath/Segment.java
@@ -0,0 +1,46 @@
+// ------------------------------------------------------------------
+// Defines a class containing two place names, and the distance 
+// between the places
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: java Segment
+//
+// ------------------------------------------------------------------
+
+public class Segment {
+
+  // define attributes
+  private String startingPoint;        // name of the starting point
+  private String endpoint;             // name of the endpoint
+  private int distance;                // distance between the points
+
+  int getDistance () {
+    return distance;                   // return the stored value
+  }
+
+  void setDistance (int newValue) {
+    distance = newValue;               // assign a new value
+  }
+
+  int getStartingPoint () {
+    return startingPoint;              // return the stored name
+  }
+
+  void setStartingPoint (int newName) {
+    startingPoint = newName;           // assign a new name for the starting point
+  }
+
+  int getEndPoint () {
+    return endpoint;                   // return the stored name
+  }
+
+  void setEndPoint (int newName) {
+    endpoint = newName;                // assign a new name for the endpoint
+  }
+}
