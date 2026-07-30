faaa22f7cd73ee8c0024b0e4eee25f473705e344
Frank Hofmann <frank.hofmann@efho.de>
2026-07-30 15:46:57 +0200

    added example for comparisons using AND, full analysis

--- comparisons/BasicComparisonsLogicalAndFull.java
+++ comparisons/BasicComparisonsLogicalAndFull.java
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------------
 // Demonstrates basic comparisons in combination with logical AND
-// Use incomplete analysis (short evaluation)
+// Use complete analysis (full evaluation)
 //
 // (C) 2026 Frank Hofmann
 // Released under GNU Public License (GPL)
@@ -9,19 +9,19 @@
 // License: GNU Public License (GPL) v.3.0 or later
 // SPDX-License-Identifier: GNU General Public License v3.0 or later
 // 
-// Usage: java BasicComparisonsLogicalAnd.java
+// Usage: java BasicComparisonsLogicalAndFull.java
 // ------------------------------------------------------------------
 
-public class BasicComparisonsLogicalAnd {
+public class BasicComparisonsLogicalAndFull {
   public static void main (String[] args) {
 
     // define two values
     int data1 = 10;
     int data2 = 15;
 
-    // use && for an incomplete analysis
-    if (data1 == 10 && data2 == 15) {
-        System.out.printf("data1 has the value of 10, and data2 has 15 \n");
+    // use & for a complete analysis
+    if (data1 == 10 & data2 == 15) {
+      System.out.printf("data1 has the value of 10, and data2 has 15 \n");
     } else {
       System.out.printf("values do not match the requirements");
     }
