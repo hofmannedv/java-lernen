25e091b51a058c98ccb188de290994bb5e2da639
Frank Hofmann <frank.hofmann@efho.de>
2026-07-31 16:34:45 +0200

    how to fill an array entirely with similar values

---
+++ arrays-and-lists/FillArray.java
@@ -0,0 +1,31 @@
+// ------------------------------------------------------------------
+// Demonstrates how to fill an arry with identical values
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: java FillArray
+// ------------------------------------------------------------------
+
+import java.util.Arrays;
+
+public class FillArray {
+  public static void main (String[] args) {
+
+    // define data as list of 5 empty fields
+    int[] data = new int[5]; 
+
+    // fill the entire array with the value of 10 per field
+    Arrays.fill(data, 10);
+
+    // print the content of the array
+    for (int value: data) {
+      System.out.printf("value: %d\n", value);
+    }
+
+  }
+}
