649551a87d454a7e65057d84ad536e8eefa4c410
Frank Hofmann <frank.hofmann@efho.de>
2026-08-12 01:11:34 +0200

    added basic class that represents geometric points, and stores the points as a list

---
+++ mathematics/Point1dList.java
@@ -0,0 +1,43 @@
+// ------------------------------------------------------------------
+// Demonstrates how to use a prevously defined Point class for 1D
+// and create a list of 1D points
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: javac Point1dList.java
+//        java Point1dList
+// ------------------------------------------------------------------
+
+import geometry.Point1d;
+import java.util.ArrayList;
+
+public class Point1dList {
+    public static void main(String[] args) {
+
+        // define data to work with
+        double[] data = {4.0, 17.5, 2.73, 6.5, 9.3};
+
+        // define a flexible array list of 1D points
+        ArrayList<Point1d> pointlist = new ArrayList<Point1d>();        
+
+        // process data
+        for (double value : data) {
+            // define new point based on value
+            Point1d point = new Point1d(value);
+
+            // extend list of points
+            pointlist.add(point);
+        }
+
+        // retrieve value, and print it
+        for (Point1d point : pointlist) {
+            double value = point.getX();
+            System.out.print("x value: " + value + "\n");
+        }
+    }
+}
