7093dc69a9a8837b23c1e347b9605cb7e8434da3
Frank Hofmann <frank.hofmann@efho.de>
2026-08-12 11:18:51 +0200

    added basic class that represents geometric points in 2d, test case

---
+++ mathematics/Point2dTest.java
@@ -0,0 +1,28 @@
+// ------------------------------------------------------------------
+// Demonstrates how to use a prevously defined Point class for 2D
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: javac Point2dTest.java
+//        java Point2dTest
+// ------------------------------------------------------------------
+
+import geometry.Point2d;
+
+public class Point2dTest {
+    public static void main(String[] args) {
+        // define new point, value of (4.0, 7.5)
+        Point2d point = new Point2d(4.0, 7.5);
+
+        // retrieve value, and print it
+        double x = point.getX();
+        System.out.print("x value: " + x + "\n");
+        double y = point.getY();
+        System.out.print("y value: " + y + "\n");
+    }
+}
