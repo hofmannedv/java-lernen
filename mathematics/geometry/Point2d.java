6c3eb7f5925b6ddd409fb1c0e14125c6e6866bb9
Frank Hofmann <frank.hofmann@efho.de>
2026-08-12 19:11:13 +0200

    added Point 2D example

--- mathematics/geometry/Point2d.java
+++ mathematics/geometry/Point2d.java
@@ -12,24 +12,37 @@
 // ------------------------------------------------------------------
 
 package geometry;
+import geometry.Point1d;
 
-public class Point2d extends Point1d {
-    // define additional class variable y
-    double y;
+public class Point2d {
+    // define class variables x, and y as Point1d
+    Point1d x = new Point1d(0.0);
+    Point1d y = new Point1d(0.0);
 
-    public Point2d (double valueX, valueY) {
-        // redefine constructor
-        x = valueX;
-        y = valueY;
+    public Point2d (double valueX, double valueY) {
+        // redefine constructor, and assign values for x, and y axis
+        this.x.setValue(valueX);
+        this.y.setValue(valueY);
+    }
+
+    public void setX(double value) {
+        // assign a new value to class attribute x
+        this.x.setValue(value);
     }
 
     public void setY(double value) {
-        // assign y class attribute with value
-        y = value;
+        // assign a new value to class attribute y 
+        this.y.setValue(value);
+    }
+
+    public double getX() {
+        // return the stored x value
+        return this.x.getValue();
     }
 
     public double getY() {
         // return the stored y value
-        return y;
+        return this.y.getValue();
     }
+
 }
