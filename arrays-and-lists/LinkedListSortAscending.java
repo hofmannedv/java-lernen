b82003ab1e31fe8e1c526ed1ab5408cbd1337a46
Frank Hofmann <frank.hofmann@efho.de>
2026-08-14 16:42:16 +0200

    added LinkedList as data structure, sorting in ascending order

---
+++ arrays-and-lists/LinkedListSortAscending.java
@@ -0,0 +1,40 @@
+// ------------------------------------------------------------------
+// Demonstrates how to sort a linked list in ascending order
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Inspiration by: Java LinkedList sort() Method
+// https://www.w3schools.com/java/ref_linkedlist_sort.asp
+// 
+// Usage: java LinkedListSortAscending
+// ------------------------------------------------------------------
+
+import java.util.LinkedList;
+
+public class LinkedListSortAscending {
+    public static void main(String[] args) {
+
+        // create a data set using LinkedList
+        LinkedList<String> capital = new LinkedList<String>();
+
+        // add entries
+        capital.add("Paris");
+        capital.add("Bern");
+        capital.add("Berlin");
+        capital.add("Madrid");
+        capital.add("Oslo");
+
+        // output the contents of the data set
+        System.out.println("data set (with original data):" + capital);
+
+        // sorting the linked list in ascending order
+        System.out.println("sorting the data set ...");
+        capital.sort(null);
+        System.out.println("sorted data set:" + capital);
+    }
+}
