efac42488f9149348ddaffc8b7b7f797e3d2ba36
Frank Hofmann <frank.hofmann@efho.de>
2026-08-20 23:15:10 +0200

    added reading the contents of a file, and prints all lines to stdout that contain a specified string

---
+++ filesystem/ReadfileFgrep.java
@@ -0,0 +1,56 @@
+// ------------------------------------------------------------------
+// Demonstrates how to read the contents of a file, and prints all
+// lines to standard out that contain a specified substring. Acts 
+// similar to the UNIX/Linux command fgrep.
+//
+// Required file: localfile
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// Inspired from: Java Read Files
+// https://www.w3schools.com/java/java_files_read.asp
+//
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: java ReadfileFgrep
+// ------------------------------------------------------------------
+
+// import required modules
+import java.io.File;
+import java.io.FileNotFoundException; // handle errors
+import java.util.Scanner;             // module to read text files
+
+public class ReadfileFgrep {
+  public static void main(String[] args) {
+    // define a file object to read from localfile
+    File myObj = new File("localfile");
+
+    // define line counter
+    int lineCounter = 1;
+
+    // search term: allo
+    String searchTerm = "allo";
+
+    // try to read from the file
+    try (Scanner myReader = new Scanner(myObj)) {
+      // ... as long as there are lines 
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+
+        if (data.indexOf(searchTerm) > 0) {
+            // we found a line that contains the term we look for
+            // print line to stdout
+            System.out.println(lineCounter + ": " + data);
+        }
+        lineCounter = lineCounter + 1;
+      }
+    } catch (FileNotFoundException e) {
+      // sth went wrong: file not found
+      System.out.println("Cannot read from file");
+      e.printStackTrace();
+    }
+  }
+}
