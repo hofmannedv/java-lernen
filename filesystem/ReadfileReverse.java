5ac0c4b7676f176330826c8d2147c759da0b6292
Frank Hofmann <frank.hofmann@efho.de>
2026-08-21 19:40:30 +0200

    read a file, and prints it to stdout line by line in reverse order, final version

--- filesystem/ReadfileReverse.java
+++ filesystem/ReadfileReverse.java
@@ -18,26 +18,37 @@
 // Usage: java ReadfileReverse
 // ------------------------------------------------------------------
 
-// *** work in progress ***
-
 // import required modules
 import java.io.File;
 import java.io.FileNotFoundException; // handle errors
 import java.util.Scanner;             // module to read text files
+import java.util.LinkedList;          // module to process linked lists
 
 public class ReadfileReverse {
   public static void main(String[] args) {
     // define a file object to read from localfile
     File myObj = new File("localfile");
 
+    // create a data set using LinkedList
+    LinkedList<String> fileContent = new LinkedList<String>();
+
     // try to read from the file
     try (Scanner myReader = new Scanner(myObj)) {
       // ... as long as there are lines 
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
+
+        // add current line to the linked list
+        fileContent.add(data);
+      }
+
+      // output list in reverse order -- last element first
+      while (fileContent.size() > 0) {
+        String data = fileContent.removeLast();
         // print it to stdout
         System.out.println(data);
       }
+
     } catch (FileNotFoundException e) {
       // sth went wrong: file not found
       System.out.println("Cannot read from file");
