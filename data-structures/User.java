750078d54cd7c4cb0d0ff5a80928e7c20d19e6ba
Frank Hofmann <frank.hofmann@efho.de>
2026-08-25 17:04:27 +0200

    added record, and corresponding example

---
+++ data-structures/User.java
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------
+// Demonstrates how to define a record
+//
+// (C) 2026 Frank Hofmann
+// Released under GNU Public License (GPL)
+// Email frank.hofmann@efho.de
+// 
+// License: GNU Public License (GPL) v.3.0 or later
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+// 
+// Usage: java User
+// ------------------------------------------------------------------
+
+// define record
+public record User(int id, String firstName, String lastName) {}
